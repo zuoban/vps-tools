862f1742167956cf2e4c6fae9107f629df005fc9
WangJinQiang <1061669148@qq.com>
2018-07-06 10:27:41 +0800

    修改为 war 包部署

--- src/main/java/com/zuoban/toy/vpstools/VpsToolsApplication.java
+++ src/main/java/com/zuoban/toy/vpstools/VpsToolsApplication.java
@@ -2,10 +2,17 @@ package com.zuoban.toy.vpstools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VpsToolsApplication {
+public class VpsToolsApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(VpsToolsApplication .class, args);
+        SpringApplication.run(VpsToolsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VpsToolsApplication.class);
     }
 }
