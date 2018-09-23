17bbcd92b70292c35cc19e65ce985313064f903b
WangJinQaing <1061669148@qq.com>
2018-09-23 10:50:48 +0800

    使用内置Tomcat运行

--- src/main/java/com/zuoban/toy/vpstools/VpsToolsApplication.java
+++ src/main/java/com/zuoban/toy/vpstools/VpsToolsApplication.java
@@ -2,17 +2,11 @@ package com.zuoban.toy.vpstools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VpsToolsApplication extends SpringBootServletInitializer {
+public class VpsToolsApplication{
     public static void main(String[] args) {
         SpringApplication.run(VpsToolsApplication.class, args);
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(VpsToolsApplication.class);
-    }
 }
+
