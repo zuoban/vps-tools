5dd50ac12c1f94738dcf7dfa4e5c593fe9e448bb
WangJinQaing <1061669148@qq.com>
2018-09-24 13:07:07 +0800

    重构系统， 前后端分离

--- src/main/java/com/zuoban/toy/vpstools/controller/UserFileController.java
+++ src/main/java/com/zuoban/toy/vpstools/controller/UserFileController.java
@@ -1,7 +1,9 @@
 package com.zuoban.toy.vpstools.controller;
 
+import cn.hutool.core.lang.Dict;
 import com.zuoban.toy.vpstools.entity.File;
 import com.zuoban.toy.vpstools.entity.UserFile;
+import com.zuoban.toy.vpstools.properties.AppProperties;
 import com.zuoban.toy.vpstools.service.FileService;
 import com.zuoban.toy.vpstools.service.FileStorageService;
 import com.zuoban.toy.vpstools.service.UserFileService;
@@ -9,17 +11,11 @@ import com.zuoban.toy.vpstools.supports.bean.BaseQuery;
 import com.zuoban.toy.vpstools.supports.bean.RestResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.Optional;
 
 /**
@@ -34,13 +30,13 @@ public class UserFileController {
 
     private final FileService fileService;
 
-    private final FileStorageService fileStorageService;
+    private final AppProperties appProperties;
 
     @Autowired
-    public UserFileController(UserFileService userFileService, FileService fileService, FileStorageService fileStorageService) {
+    public UserFileController(UserFileService userFileService, FileService fileService, FileStorageService fileStorageService, AppProperties appProperties) {
         this.userFileService = userFileService;
         this.fileService = fileService;
-        this.fileStorageService = fileStorageService;
+        this.appProperties = appProperties;
     }
 
     @GetMapping
@@ -50,7 +46,7 @@ public class UserFileController {
     }
 
     @GetMapping("download")
-    public ResponseEntity<Resource> download(Long id, HttpServletRequest request) {
+    public RestResponse download(Long id) {
         Optional<UserFile> userFileOptional = userFileService.findById(id);
         UserFile userFile = userFileOptional.orElseThrow(() -> new RuntimeException("文件不存在"));
 
@@ -59,25 +55,28 @@ public class UserFileController {
         Optional<File> fileOptional = fileService.findById(fileId);
         File file = fileOptional.orElseThrow(() -> new RuntimeException("文件不存在"));
 
+        Dict result = new Dict(2).set("path", file.getPath()).set("urlPrefix", appProperties.getUrlPrefix());
 
-        Resource resource = fileStorageService.loadFileAsResource(file.getPath());
+        return RestResponse.success(result);
 
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            log.info("Could not determine file type.");
-        }
-
-        // Fallback to the default content type if type could not be determined
-        if (contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+//        Resource resource = fileStorageService.loadFileAsResource(file.getPath());
+//
+//        // Try to determine file's content type
+//        String contentType = null;
+//        try {
+//            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+//        } catch (IOException ex) {
+//            log.info("Could not determine file type.");
+//        }
+//
+//        // Fallback to the default content type if type could not be determined
+//        if (contentType == null) {
+//            contentType = "application/octet-stream";
+//        }
+//
+//        return ResponseEntity.ok()
+//                .contentType(MediaType.parseMediaType(contentType))
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+//                .body(resource);
     }
 }
